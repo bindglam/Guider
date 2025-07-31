bb0513f6343f79e21af4ace7ecf7ad37c6171bb9
Woobeen Jeon <woobeen0516@gmail.com>
2025-07-31 17:22:35 +0900

    리팩토링

--- core/src/main/kotlin/com/bindglam/guider/manager/PlayerDataManagerImpl.kt
+++ core/src/main/kotlin/com/bindglam/guider/manager/PlayerDataManagerImpl.kt
@@ -9,17 +9,19 @@ import java.util.concurrent.TimeUnit
 class PlayerDataManagerImpl(private val plugin: Plugin, private val vertexManager: VertexManager, private val navigationManager: NavigationManager) : PlayerDataManager {
     override fun init() {
         DatabaseUtils.connection.use { connection ->
-            val statement = connection.createStatement()
-            statement.execute("CREATE TABLE IF NOT EXISTS guider_playerdata(uuid VARCHAR(36) PRIMARY KEY, destination TINYTEXT)")
-            statement.close()
+            connection.createStatement().apply {
+                execute("CREATE TABLE IF NOT EXISTS guider_playerdata(uuid VARCHAR(36) PRIMARY KEY, destination TINYTEXT)")
+                close()
+            }
         }
     }
 
     override fun load(player: Player) {
         Bukkit.getAsyncScheduler().runDelayed(plugin, { task ->
             DatabaseUtils.connection.use { connection ->
-                val statement = connection.prepareStatement("SELECT * FROM guider_playerdata WHERE uuid = ?")
-                statement.setString(1, player.uniqueId.toString())
+                val statement = connection.prepareStatement("SELECT * FROM guider_playerdata WHERE uuid = ?").apply {
+                    setString(1, player.uniqueId.toString())
+                }
                 val rs = statement.executeQuery()
 
                 if(rs.next()) {
@@ -43,25 +45,32 @@ class PlayerDataManagerImpl(private val plugin: Plugin, private val vertexManage
 
         Bukkit.getAsyncScheduler().runNow(plugin) { task ->
             DatabaseUtils.connection.use { connection ->
-                val statement1 = connection.prepareStatement("SELECT * FROM guider_playerdata WHERE uuid = ?")
-                statement1.setString(1, player.uniqueId.toString())
+                val statement1 = connection.prepareStatement("SELECT * FROM guider_playerdata WHERE uuid = ?").apply {
+                    setString(1, player.uniqueId.toString())
+                }
                 val rs = statement1.executeQuery()
 
                 if (destination == null && rs.next()) {
-                    val statement2 = connection.prepareStatement("DELETE FROM guider_playerdata WHERE uuid = ?")
-                    statement2.setString(1, player.uniqueId.toString())
+                    val statement2 = connection.prepareStatement("DELETE FROM guider_playerdata WHERE uuid = ?").apply {
+                        setString(1, player.uniqueId.toString())
+                    }
                     statement2.executeUpdate()
                     statement2.close()
-                } else if (destination != null && rs.next()) {
-                    val statement2 = connection.prepareStatement("UPDATE guider_playerdata SET destination = ? WHERE uuid = ?")
-                    statement2.setString(1, destination)
-                    statement2.setString(2, player.uniqueId.toString())
-                    statement2.executeUpdate()
-                    statement2.close()
-                } else if (destination != null) {
-                    val statement2 = connection.prepareStatement("INSERT INTO guider_playerdata(uuid, destination) VALUES (?, ?)")
-                    statement2.setString(1, player.uniqueId.toString())
-                    statement2.setString(2, destination)
+                }
+
+                if (destination != null) {
+                    val statement2 = if(rs.next()) {
+                        connection.prepareStatement("UPDATE guider_playerdata SET destination = ? WHERE uuid = ?").apply {
+                            setString(1, destination)
+                            setString(2, player.uniqueId.toString())
+                        }
+                    } else {
+                        connection.prepareStatement("INSERT INTO guider_playerdata(uuid, destination) VALUES (?, ?)").apply {
+                            setString(1, player.uniqueId.toString())
+                            setString(2, destination)
+                        }
+                    }
+
                     statement2.executeUpdate()
                     statement2.close()
                 }
